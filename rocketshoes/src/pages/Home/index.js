fde2858e9214f2f1db27759a2861eb6ece4acbe8
Shilton <shilton.sistemas@gmail.com>
2020-02-02 20:29:53 -0300

    Creating and styling Home

--- rocketshoes/src/pages/Home/index.js
+++ rocketshoes/src/pages/Home/index.js
@@ -1,6 +1,99 @@
 import React from 'react';
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-station-masculino/26/AXB-1666-226/AXB-1666-226_detalhe2.jpg?ims=326x"
+          alt="imgProduto"
+        />
+        <strong>Tênis Top</strong>
+        <span>R$ 129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-station-masculino/26/AXB-1666-226/AXB-1666-226_detalhe2.jpg?ims=326x"
+          alt="imgProduto"
+        />
+        <strong>Tênis Top</strong>
+        <span>R$ 129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-station-masculino/26/AXB-1666-226/AXB-1666-226_detalhe2.jpg?ims=326x"
+          alt="imgProduto"
+        />
+        <strong>Tênis Top</strong>
+        <span>R$ 129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-station-masculino/26/AXB-1666-226/AXB-1666-226_detalhe2.jpg?ims=326x"
+          alt="imgProduto"
+        />
+        <strong>Tênis Top</strong>
+        <span>R$ 129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-station-masculino/26/AXB-1666-226/AXB-1666-226_detalhe2.jpg?ims=326x"
+          alt="imgProduto"
+        />
+        <strong>Tênis Top</strong>
+        <span>R$ 129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-station-masculino/26/AXB-1666-226/AXB-1666-226_detalhe2.jpg?ims=326x"
+          alt="imgProduto"
+        />
+        <strong>Tênis Top</strong>
+        <span>R$ 129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
